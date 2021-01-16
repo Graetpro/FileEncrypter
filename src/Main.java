30ea724ae66fa886b970bd4e44c8ea5a4cc2e901
Graetpro X <36183421+Graetpro@users.noreply.github.com>
2021-01-16 18:37:18 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,46 @@
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, InvalidKeySpecException, InvalidKeyException, IOException {
+
+        if(args.length != 3){
+            argerr();
+        }else {
+            if(args[0].equals("-e")) {
+                Scanner scanner = new Scanner(System.in);
+                if (Locale.getDefault().getLanguage().equals("zh")){
+                    System.out.print("请输入加密密钥：");
+                }else {
+                    System.out.print("Please enter the encryption key:");
+                }
+                encrypt(scanner.nextLine(),args[1],args[2]);
+            }else if(args[0].equals("-d")){
+                Scanner scanner = new Scanner(System.in);
+                if (Locale.getDefault().getLanguage().equals("zh")){
+                    System.out.print("请输入解密密钥：");
+                }else {
+                    System.out.print("Please enter the decryption key:");
+                }
+                decrypt(scanner.nextLine(),args[1],args[2]);
+            }else{
+                argerr();
+            }
+        }
+
+
+
 //        readFile("D:\\Xdministrator\\Desktop\\a.jpg");
 /*        try {
             convertToHex(System.out, new File("D:\\Xdministrator\\Desktop\\a.jpg"));
@@ -39,8 +71,20 @@ public class Main {
 //        hexWrite(hexString2Bytes(Des.encrypt(hexReadFile("D:\\Xdministrator\\Desktop\\a.jpg"),Des.KEY)),"D:\\Xdministrator\\Desktop\\a.jpg.des");
 
 //        hexWrite(hexString2Bytes(Des.decrypt(hexReadFile("D:\\Xdministrator\\Desktop\\a.jpg.des"),Des.KEY)),"D:\\Xdministrator\\Desktop\\x.jpg");
-        hexWrite(hexString2Bytes(Des.decrypt(hexReadFile("D:\\Xdministrator\\Desktop\\a.jpg.des"),Des.KEY)),"D:\\Xdministrator\\Desktop\\x.jpg");
-//        hexWrite(hexString2Bytes(Des.decrypt(readFile("D:\\Xdministrator\\Desktop\\a.jpg.des"),Des.KEY)),"D:\\Xdministrator\\Desktop\\x.jpg");
+
+
+
+
+
+
+
+        //hexWrite(hexString2Bytes(Des.decrypt(hexReadFile("D:\\Xdministrator\\Desktop\\a.jpg.des"),Des.KEY)),"D:\\Xdministrator\\Desktop\\x.jpg");
+
+
+
+
+
+                //hexWrite(hexString2Bytes(Des.decrypt(readFile("D:\\Xdministrator\\Desktop\\a.jpg.des"),Des.KEY)),"D:\\Xdministrator\\Desktop\\x.jpg");
 //        System.out.println(readFile("D:\\\\Xdministrator\\\\Desktop\\\\a.jpg"));
        /* byte[] r = new byte[]{(byte)0x46,(byte)0x55,(byte)0x43,(byte)0x4b,(byte)0x20,(byte)0x55};
         write(r);*/
@@ -48,6 +92,22 @@ public class Main {
 //        System.out.println(hexReadFile("D:\\Xdministrator\\Desktop\\a.jpg"));
     }
 
+    public static void encrypt(String key,String inputFilePath,String outputFilePath) throws IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, InvalidKeySpecException, InvalidKeyException, IOException {
+        hexWrite(hexString2Bytes(Des.encrypt(hexReadFile(inputFilePath),key)),outputFilePath);
+    }
+
+    public static void decrypt(String key,String inputFilePath,String outputFilePath) throws IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, InvalidKeySpecException, InvalidKeyException, IOException {
+        hexWrite(hexString2Bytes(Des.decrypt(hexReadFile(inputFilePath),key)),outputFilePath);
+
+        //hexWrite(Des.decrypt(hexReadFile(inputFilePath),key).getBytes(),outputFilePath);
+    }
+
+    public static void argerr(){
+        if(Locale.getDefault().getLanguage().equals("zh"))
+            System.out.print("请输入要加密或解密（加密为 -e，解密为 -d）、要加密或解密的文件路径和输出文件的路径。");
+        else
+            System.out.print("Please enter the encryption or decryption (-e for encryption, -d for decryption), the path of the file to be encrypted or decrypted, and the path of the output file.");
+    }
     /*
      * 16进制字符串转字节数组
      */
@@ -55,11 +115,9 @@ public class Main {
 
         if ((hex == null) || (hex.equals(""))){
             return null;
-        }
-        else if (hex.length()%2 != 0){
+        }else if (hex.length()%2 != 0){
             return null;
-        }
-        else{
+        }else{
             hex = hex.toUpperCase();
             int len = hex.length()/2;
             byte[] b = new byte[len];
@@ -80,7 +138,7 @@ public class Main {
         return (byte) "0123456789ABCDEF".indexOf(c);
     }
 
-    public static int toDec(String s) {
+    /*public static int toDec(String s) {
         int result = 0;
         for(int i = 0; i < 8; i++) {
             int temp = s.charAt(i);
@@ -90,17 +148,17 @@ public class Main {
             }else {
                 temp -= 48;
             }
-            result += temp * getProduct(7 - i);
+            result += temp * getProduct(16,7 - i);
         }
         return result;
-    }
+    }*/
     /**
-     * 计算16的n次方　
-     */
-    public static int getProduct(int n) {
+     * 计算number的n次方　
+    */
+    /*public static int getProduct(int number,int n) {
         int result = 1;
         for(int i = 0; i < n; i++) {
-            result *= 16;
+            result *= number;
         }
         return result;
     }
@@ -118,9 +176,9 @@ public class Main {
             e.printStackTrace();
         }
         return new String(bytes);
-    }
+    }*/
 
-    public static String hexReadFile( String file) throws IOException {
+    public static String hexReadFile(String file) throws IOException {
         InputStream is = new FileInputStream(new File(file));
 
         int bytesCounter =0;
@@ -186,7 +244,7 @@ public class Main {
             fop.flush();
             fop.close();
 
-            System.out.println(new String(bytes));
+            //System.out.println(new String(bytes));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -203,7 +261,7 @@ public class Main {
     /**
      * 使用FileWriter类写文本文件
      */
-    public static void writeMethod1(String file,String content)
+    /*public static void writeMethod1(String file,String content)
     {
         String fileName=file;
         try
@@ -217,13 +275,13 @@ public class Main {
         {
             e.printStackTrace();
         }
-    }
+    }*/
     //注意：上面的例子由于写入的文本很少，使用FileWrite类就可以了。但如果需要写入的
     //内容很多，就应该使用更为高效的缓冲器流类BufferedWriter。
     /**
      * 使用BufferedWriter类写文本文件
      */
-    public static void writeMethod3(String file,String content) {
+    /*public static void writeMethod3(String file,String content) {
         String fileName = file;
         try {
             BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
@@ -233,5 +291,5 @@ public class Main {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-    }
+    }*/
 }
